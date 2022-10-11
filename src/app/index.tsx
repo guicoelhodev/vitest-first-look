4fc81f25008ec2e6f390349ac4e2040ba12b2d7f
Guilherme Coelho <guilherme.coelho@veredaeducacao.com>
2022-10-11 11:10:09 -0300

    feat: create App component to render the application

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+import { Counter } from "../components/Counter";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Counter />
+    </div>
+  );
+};
