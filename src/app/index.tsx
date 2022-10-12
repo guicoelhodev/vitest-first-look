d1a56cde18c92213d2bc645bd94974cbf5492508
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-12 20:06:14 -0300

    feat: import ListName container

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import "./style.scss";
 import { CounterComponent } from "../components/Counter";
+import { ListName } from "../components/ListName";
 
 export const App = () => {
   return (
     <div className="app">
       <CounterComponent />
+      <ListName />
     </div>
   );
 };
