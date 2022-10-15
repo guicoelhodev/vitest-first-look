465660b0ce6128db0dcfc26d316ed813798ccff9
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-15 13:19:25 -0300

    test: create all tests needed to validated ListName component

--- src/components/ListName/__tests__/listName.spec.tsx
+++ src/components/ListName/__tests__/listName.spec.tsx
@@ -1,11 +1,40 @@
 import { describe } from "vitest";
 import { ListName } from "..";
-import { render, screen } from "../../../utils/test-utlis";
+import { fireEvent, render, screen } from "../../../utils/test-utlis";
 
 describe("validate listName component", () => {
   it("should return message if listName is empty", () => {
-    render(<ListName />);
+    const component = render(<ListName />);
+    expect(component.getByLabelText("emptyAdvice")).toBeInTheDocument();
+  });
 
-    expect(screen.getByTitle("emptyAdvice")).toBeInTheDocument();
+  it("should type a new Name into input", () => {
+    const component = render(<ListName />);
+    const inputName = component.getByLabelText("input-name");
+
+    fireEvent.change(inputName, { target: { value: "Rafaela" } });
+    expect(inputName).toHaveValue("Rafaela");
+  });
+
+  it("should be empty input value when add button is clicked", () => {
+    const component = render(<ListName />);
+    const inputName = component.getByLabelText("input-name");
+    const addButton = component.getByLabelText("button-addName");
+
+    fireEvent.change(inputName, { target: { value: "Rafaela" } });
+    fireEvent.click(addButton);
+
+    expect(inputName).toHaveValue("");
+  });
+
+  it("should be show the current Name into ul list", () => {
+    const component = render(<ListName />);
+    const inputName = component.getByLabelText("input-name");
+    const addButton = component.getByLabelText("button-addName");
+
+    fireEvent.change(inputName, { target: { value: "Rafaela" } });
+    fireEvent.click(addButton);
+
+    expect(component.getByText("Rafaela")).toBeInTheDocument();
   });
 });
